ace59a76f3af404df4fc360656b15094b3a1612e
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-26 01:08:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
     attr_reader :brand
-    attr_writer :color, :size, :material, :condition
+    attr_accessor :color, :size, :material, :condition
   def initialize(brand)
     @brand = brand
   end
